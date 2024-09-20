a4a895b59110eb92334d26861ac669d70becde8a
dylanebert <dylan@huggingface.co>
2024-09-20 13:51:20 -0400

    fix output

--- examples/convert.py
+++ examples/convert.py
@@ -1,8 +1,7 @@
 from instant_texture import Converter
 
 
-input_mesh_path = "inputs/chair.obj"
-output_mesh_path = "outputs/chair.glb"
+input_mesh_path = "chair.obj"
 
 converter = Converter()
-converter.convert(input_mesh_path, output_mesh_path)
+converter.convert(input_mesh_path)
